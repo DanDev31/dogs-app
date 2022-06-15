9403c17bcce6a8226b30098ed5b32f4952a8569f
Daniel Sánchez <juandaniel.25@outlook.com>
2022-06-15 17:08:37 -0500

    Updated

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ conn.sync({ force: true }).then(() => {
       })
   })
   })
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
