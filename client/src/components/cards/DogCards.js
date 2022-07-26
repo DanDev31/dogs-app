b67c07a8d376f7a208c7e1dc2a166fa4dfd7ca98
Juan Daniel Sánchez <81533340+DanDev31@users.noreply.github.com>
2022-07-26 10:02:48 -0500

    Update DogCards.js

--- client/src/components/cards/DogCards.js
+++ client/src/components/cards/DogCards.js
@@ -34,7 +34,7 @@ export const DogCards = () => {
     <section>
       {
         loading ?
-          (dogs.length > 0 ?
+          (dogs?.length > 0 ?
             <>
             <div className='cards__container'>
               {
